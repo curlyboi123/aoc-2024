8fcbf5699da63b37c1687e214d019d6cc2927148
John Colton <jcolton96@gmail.com>
2025-11-05 14:49:34 +0000

    day 2 refactoring

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
+
 use std::io::{self, BufRead};
+
 use std::path::Path;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -7,42 +9,77 @@ where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
+
     Ok(io::BufReader::new(file).lines())
 }
 
 #[derive(PartialEq, Debug)]
+
 enum Direction {
     Increasing,
+
     Decreasing,
 }
 
 fn main() {
     let mut safe_count: i32 = 0;
 
-    if let Ok(lines) = read_lines("../inputs/02/short.txt") {
+    if let Ok(lines) = read_lines("../inputs/02/long.txt") {
         for line in lines.map_while(Result::ok) {
             let mut is_safe = true;
+
             let mut direction: Option<Direction> = None;
+
             // println!("{:?}", line);
+
             let report_levels: Vec<i32> = line
                 .split_whitespace()
                 .map(|x| x.parse::<i32>().unwrap())
                 .collect();
 
-            println!("Line {line}");
+            let groups = report_levels.windows(2);
 
-            let groups_of_three = report_levels.windows(3);
-            // for level in report_levels.iter() {
-            //     println!("{level}");
-            // }
+            for i in groups {
+                let diff = (i[1] - i[0]).abs();
 
-            println!("groups of three: {:?}", groups_of_three);
+                if diff < 1 || diff > 3 {
+                    is_safe = false;
 
-            let initial_direction: Direction;
+                    break;
+                }
 
-            for group in groups_of_three {
-                println!("Group: {:?}", group);
+                match direction {
+                    Some(Direction::Increasing) => {
+                        if i[1] < i[0] {
+                            is_safe = false;
+
+                            break;
+                        }
+                    }
+
+                    Some(Direction::Decreasing) => {
+                        if i[1] > i[0] {
+                            is_safe = false;
+
+                            break;
+                        }
+                    }
+
+                    None => {
+                        if i[1] > i[0] {
+                            direction = Some(Direction::Increasing)
+                        } else {
+                            direction = Some(Direction::Decreasing)
+                        }
+                    }
+                }
+            }
+
+            if is_safe {
+                safe_count += 1;
             }
         }
     }
+
+    println!("Safe count: {}", safe_count);
 }
