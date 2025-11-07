b30e3a980db8ad47e62d081c1a8b26881c86a89f
John Colton <jcolton96@gmail.com>
2025-11-07 15:48:27 +0000

    Format day 2 and add day 3

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,7 +1,5 @@
 use std::fs::File;
-
 use std::io::{self, BufRead};
-
 use std::path::Path;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -9,15 +7,12 @@ where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-
     Ok(io::BufReader::new(file).lines())
 }
 
 #[derive(PartialEq, Debug)]
-
 enum Direction {
     Increasing,
-
     Decreasing,
 }
 
@@ -27,10 +22,7 @@ fn main() {
     if let Ok(lines) = read_lines("../inputs/02/long.txt") {
         for line in lines.map_while(Result::ok) {
             let mut is_safe = true;
-
             let mut direction: Option<Direction> = None;
-
-            // println!("{:?}", line);
 
             let report_levels: Vec<i32> = line
                 .split_whitespace()
